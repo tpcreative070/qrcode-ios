c612ef370d353aa7b6b7c92d08e4e2573c0acc6a
Mr Phong <ttphong@ecpay.vn>
2020-02-21 11:31:06 +0700

    Initial Commit

---
+++ qrscanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  qrscanner
+//
+//  Created by phong070 on 2/21/20.
+//  Copyright © 2020 thanhphong070. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
