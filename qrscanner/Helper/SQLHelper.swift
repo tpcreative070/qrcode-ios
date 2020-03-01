4274a014680e07449f658d951641a8d9b734ec5d
Mr Phong <ttphong@ecpay.vn>
2020-03-01 21:09:27 +0700

    [feature-163629022020] - Changed file name

--- qrscanner/Helper/SQLHelper.swift
+++ qrscanner/Helper/SQLHelper.swift
@@ -39,7 +39,7 @@ class SQLHelper {
     }
     
     class open func initCipher(isDelete : Bool? = nil){
-        guard let mData = DocumentHelper.loadBundle(fileName: "eWalletCipher", mExtension: FolderName.db) else {
+        guard let mData = DocumentHelper.loadBundle(fileName: "qrscanner", mExtension: FolderName.db) else {
             debugPrint("Not found...")
             return
         }
@@ -47,20 +47,20 @@ class SQLHelper {
     }
     
     class open func writeFile(data : Data,isDelete : Bool? = nil){
-        let fileName = "eWalletCipher.db"
+        let fileName = "qrscanner.db"
         if let _ = isDelete {
             DocumentHelper.createdFile(data: data, folderName: FolderName.db,fileName: fileName)
             return
         }
         guard let _ = DocumentHelper.getFilePath(fileName: fileName,folderName: FolderName.db) else {
             DocumentHelper.createdFile(data: data, folderName: FolderName.db,fileName: fileName)
-            debugPrint("eWalletCipher.db was created")
+            debugPrint("qrscanner.db was created")
             return
         }
     }
     
     class open func getPathFile() -> Bool{
-        let fileName = "eWalletCipher.db"
+        let fileName = "qrscanner.db"
         guard let _ = DocumentHelper.getFilePath(fileName: fileName,folderName: FolderName.db) else {
             return false
         }
@@ -68,7 +68,7 @@ class SQLHelper {
     }
     
     class open func connection() -> Connection?{
-        let fileName = "eWalletCipher.db"
+        let fileName = "qrscanner.db"
         guard let mUrl = DocumentHelper.getFilePath(fileName: fileName,folderName: FolderName.db) else {
             return nil
         }
